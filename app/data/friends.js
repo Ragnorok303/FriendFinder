292e5bfdb05f00bda5044ac4ddec1bc91a030436
Ragnorok303 <jasontompkins22@gmail.com>
2020-04-23 18:08:47 -0600

    Created API database

---
+++ app/data/friends.js
@@ -0,0 +1,85 @@
+var friends = [
+
+    {
+      name: 'Batman',
+      photo: 'https://img.cinemablend.com/filter:scale/quill/e/5/c/2/4/9/e5c2494b7fec13181defbc8afe66a7a3bee5bae0.jpg?mw=600',
+      scores: [
+        '5',
+        '1',
+        '4',
+        '3',
+        '1',
+        '5',
+        '1',
+        '4',
+        '3',
+        '1'
+      ]
+    },
+    {
+      name: 'Aquaman',
+      photo: 'https://vignette.wikia.nocookie.net/dccu/images/a/a9/Aquaman_Arthur_Curry_Character_Textless_Poster.jpg/revision/latest/scale-to-width-down/337?cb=20181123170241',
+      scores: [
+        '3',
+        '1',
+        '5',
+        '4',
+        '1',
+        '5',
+        '1',
+        '1',
+        '5',
+        '3'
+      ]
+    },
+    {
+      name: 'Wonder Women',
+      photo: 'https://www.actualitte.com/images/actualites/images/newsletter%20bayard%20rain/Wonder_Woman.jpg',
+      scores: [
+        '3',
+        '1',
+        '4',
+        '1',
+        '4',
+        '3',
+        '5',
+        '1',
+        '5',
+        '4'
+      ]
+    },
+    {
+      name: 'The Flash',
+      photo: 'https://vignette.wikia.nocookie.net/injustice-2-mobile2242/images/2/28/Ce3ada9874e05c01d1211d17cca313b3.jpg/revision/latest/scale-to-width-down/220?cb=20190610163529',
+      scores: [
+        '2',
+        '3',
+        '2',
+        '5',
+        '4',
+        '5',
+        '1',
+        '2',
+        '3',
+        '5'
+      ]
+    },
+    {
+      name: 'Superman',
+      photo: 'https://consequenceofsound.net/wp-content/uploads/2018/09/henry-cavill-out-as-superman-dc-extended-universe-warner-bros.png',
+      scores: [
+        '4',
+        '3',
+        '4',
+        '4',
+        '1',
+        '5',
+        '5',
+        '2',
+        '2',
+        '1'
+      ]
+    }
+  ]
+  
+  module.exports = friends
